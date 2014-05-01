f2c4cb1adf77e543bd13bc08f3b04e99ab4cdd5d
hidykong <hidykong@gmail.com>
2014-04-30 23:17:56 -0500

    fixed history encoding issues

--- visualization/keywords.js
+++ visualization/keywords.js
@@ -13,15 +13,21 @@ ajax.open("GET", "political.json", false);
 ajax.send(null);
 var political = JSON.parse(ajax.responseText);
 
+console.log(history[0].content);
+console.log(food[0].content);
+console.log(political[0].content);
 
 function getPost(category, year, keyword) {
   switch(category){
     case 'history':
       var result = $.grep(history, function(e){ return e.year == year; }); //find all matching years
+      break;
     case 'food':
       var result = $.grep(food, function(e){ return e.year == year; });
+      break;
     case 'political':
-      var result = $.grep(political, function(e){ return e.year == year; });  
+      var result = $.grep(political, function(e){ return e.year == year; });
+      break;  
   }
 
   $.each(result, function(index, value) {
@@ -31,8 +37,8 @@ function getPost(category, year, keyword) {
         $("#kwSentences").val($("#kwSentences").val() + "\n\n" +  sentence);
       }
   });
-  
 }
 
-getPost('food', 1990, 'Camp');
+
+getPost('food', 1990, 'food');
 
